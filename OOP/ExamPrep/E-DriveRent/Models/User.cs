b2b61f145966a624079ef21b1e6fbabcbddeed5e
Denitsa <denitsabebrevenska@gmail.com>
2024-03-07 12:59:00 +0200

    User structure added

---
+++ OOP/ExamPrep/E-DriveRent/Models/User.cs
@@ -0,0 +1,87 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Utilities.Messages;
+using System;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _drivingLicenseNumber;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DrivingLicenseNumber = drivingLicenseNumber;
+            Rating = 0;
+            IsBlocked = false;
+        }
+        public string FirstName
+        {
+            get => _firstName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.FirstNameNull);
+                }
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.LastNameNull);
+                }
+                _lastName = value;
+            }
+        }
+
+        public double Rating { get; private set; }
+
+        public string DrivingLicenseNumber
+        {
+            get => _drivingLicenseNumber;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.DrivingLicenseRequired);
+                }
+                _drivingLicenseNumber = value;
+            }
+        }
+        public bool IsBlocked { get; private set; }
+
+        public void IncreaseRating()
+        {
+            Rating += 0.5;
+
+            if (Rating > 10)
+            {
+                Rating = 10;
+            }
+        }
+
+        public void DecreaseRating()
+        {
+            Rating -= 2;
+
+            if (Rating < 0)
+            {
+                Rating = 0;
+                IsBlocked = true;
+            }
+        }
+
+        public override string ToString()
+            => $"{FirstName} {LastName} Driving license: {DrivingLicenseNumber} Rating: {Rating}";
+    }
+}
