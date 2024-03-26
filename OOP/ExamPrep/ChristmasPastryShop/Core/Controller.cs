79975cbbe5b413b9c63a0b35732a4911d258deb5
Denitsa <denitsabebrevenska@gmail.com>
2024-03-26 23:54:14 +0200

    started working on controller

---
+++ OOP/ExamPrep/ChristmasPastryShop/Core/Controller.cs
@@ -0,0 +1,56 @@
+﻿using ChristmasPastryShop.Core.Contracts;
+using ChristmasPastryShop.Models.Booths;
+using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Repositories;
+using ChristmasPastryShop.Repositories.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+
+namespace ChristmasPastryShop.Core
+{
+    public class Controller : IController
+    {
+        private IRepository<IBooth> booths;
+        public Controller()
+        {
+            booths = new BoothRepository();
+        }
+        public string AddBooth(int capacity)
+        {
+            int id = booths.Models.Count + 1;
+            booths.AddModel(new Booth(id, capacity));
+
+            return string.Format(OutputMessages.NewBoothAdded, id, capacity);
+        }
+
+        public string AddDelicacy(int boothId, string delicacyTypeName, string delicacyName)
+        {
+            //todo implement a factory and then the rest of methods
+        }
+
+        public string AddCocktail(int boothId, string cocktailTypeName, string cocktailName, string size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReserveBooth(int countOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TryOrder(int boothId, string order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LeaveBooth(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BoothReport(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
