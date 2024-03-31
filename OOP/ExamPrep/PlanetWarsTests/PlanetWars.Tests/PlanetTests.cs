df6cc0e11a5b7bbc0df0455e3d7794b184440724
Denitsa <denitsabebrevenska@gmail.com>
2024-03-31 10:20:18 +0300

    adding some unit tests

---
+++ OOP/ExamPrep/PlanetWarsTests/PlanetWars.Tests/PlanetTests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace PlanetWars.Tests
+{
+    public class PlanetTests
+    {
+        private Planet planet;
+        private string name = "Melmac";
+        private double budget = 2000;
+        private Weapon weapon1;
+        private Weapon weapon2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            planet = new Planet(name, budget);
+            weapon1 = new Weapon("Nuclear", 30, 200);
+            weapon2 = new Weapon("SpaceGrenade", 2, 4);
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializePlanet()
+        {
+            Assert.IsNotNull(planet);
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeWeaponList()
+        {
+            Assert.IsNotNull(planet.Weapons);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetNameCorrectly()
+        {
+            Assert.AreEqual(name, planet.Name);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void SettingNameToNullOrEmpty_ShouldThrowException(string invalidName)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                planet = new Planet(invalidName, budget));
+            Assert.AreEqual("Invalid planet Name", exception.Message);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetBudgetCorrectly()
+        {
+            Assert.AreEqual(budget, planet.Budget);
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-100)]
+        public void SettingBudgetToNegativeNumber_ShouldThrowException(double invalidBudget)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                planet = new Planet(name, invalidBudget));
+            Assert.AreEqual("Budget cannot drop below Zero!", exception.Message);
+        }
+
+        //todo weapon collection + militaryRatio
+
+        [TestCase(0)]
+        [TestCase(20)]
+        [TestCase(200)]
+        public void Profit_ShouldIncreaseBudget(double profitAmount)
+        {
+            planet.Profit(profitAmount);
+            Assert.AreEqual(budget + profitAmount, planet.Budget);
+        }
+
+        [TestCase(0)]
+        [TestCase(20)]
+        [TestCase(2000)]
+        public void SpendFunds_ShouldDecreaseBudget(double decreaseAmount)
+        {
+            planet.SpendFunds(decreaseAmount);
+            Assert.AreEqual(budget - decreaseAmount, planet.Budget);
+        }
+
+        [TestCase(2001)]
+        [TestCase(3000)]
+        [TestCase(20000)]
+        public void SpendFunds_ShouldThrowException_WhenInsufficientBudget(double decreaseAmount)
+        {
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() =>
+                planet.SpendFunds(decreaseAmount));
+            Assert.AreEqual("Not enough funds to finalize the deal.", exception.Message);
+        }
+
+        [Test]
+        public void AddWeapon_ShouldAddItToTheWeaponList()
+        {
+            Assert.IsFalse(planet.Weapons.Contains(weapon1));
+            planet.AddWeapon(weapon1);
+            Assert.IsTrue(planet.Weapons.Contains(weapon1));
+        }
+        [Test]
+        public void AddWeapon_ShouldThrowException_WhenTryingToAddWeaponThatAlreadyExists()
+        {
+            planet.AddWeapon(weapon1);
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() =>
+                planet.AddWeapon(weapon1));
+            Assert.AreEqual($"There is already a {weapon1.Name} weapon.", exception.Message);
+        }
+    }
+}
---
+++ OOP/ExamPrep/PlanetWarsTests/PlanetWars.Tests/WeaponTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PlanetWars.Tests
+{
+    public class WeaponTests
+    {
+        private Weapon weapon;
+        private Weapon weapon2;
+        private string name = "Nuclear";
+        private double price = 30;
+        private int destructionLevel = 200;
+        private int lesserDestructionLevel = 2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            weapon = new Weapon(name, price, destructionLevel);
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeWeapon()
+        {
+            Assert.IsNotNull(weapon);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetNameCorrectly()
+        {
+            Assert.AreEqual(name, weapon.Name);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetPriceCorrectly()
+        {
+            Assert.AreEqual(price, weapon.Price);
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-100)]
+        public void SettingPriceToNegativeNumber_ShouldThrowException(int invalidPrice)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+                weapon2 = new Weapon(name, invalidPrice, destructionLevel));
+            Assert.AreEqual("Price cannot be negative.", exception.Message);
+        }
+
+        [Test]
+        public void Constructor_ShouldSetDestructionLevelCorrectly()
+        {
+            Assert.AreEqual(destructionLevel, weapon.DestructionLevel);
+        }
+
+        [Test]
+        public void IncreaseDestructionLevel_ShouldIncrementDestructionLevel()
+        {
+            weapon.IncreaseDestructionLevel();
+            Assert.AreEqual(++destructionLevel, weapon.DestructionLevel);
+            weapon.IncreaseDestructionLevel();
+            Assert.AreEqual(++destructionLevel, weapon.DestructionLevel);
+        }
+
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(300)]
+        public void IsNuclear_ShouldReturnTrue_WhenDestructionLevelEqualOrOver10(int nuclearDestructionLevel)
+        {
+            weapon2 = new Weapon(name, price, nuclearDestructionLevel);
+            Assert.IsTrue(weapon2.IsNuclear);
+        }
+
+        [TestCase(9)]
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void IsNuclear_ShouldReturnFalse_WhenDestructionLevelLessThan10(int nonNuclearDestructionLevel)
+        {
+            weapon2 = new Weapon(name, price, nonNuclearDestructionLevel);
+            Assert.IsFalse(weapon2.IsNuclear);
+        }
+    }
+}
