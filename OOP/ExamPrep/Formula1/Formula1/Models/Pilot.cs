2804ab80a1713de638fdb51d353517efd0ae0ce0
Denitsa <denitsabebrevenska@gmail.com>
2024-04-03 19:38:06 +0300

    Structure Formula1 added

---
+++ OOP/ExamPrep/Formula1/Formula1/Models/Pilot.cs
@@ -0,0 +1,48 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+
+namespace Formula1.Models
+{
+    public class Pilot : IPilot
+    {
+        private string fullName;
+
+        public Pilot(string fullName)
+        {
+            FullName = fullName;
+            CanRace = false;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPilot, value));
+                }
+            }
+        }
+        public IFormulaOneCar Car { get; private set; }
+        public int NumberOfWins { get; private set; }
+        public bool CanRace { get; private set; }
+        public void AddCar(IFormulaOneCar car)
+        {
+            if (car is null)
+            {
+                throw new NullReferenceException(ExceptionMessages.InvalidCarForPilot);
+            }
+
+            Car = car;
+            CanRace = true;
+        }
+
+        public void WinRace()
+            => NumberOfWins++;
+
+        public override string ToString()
+            => $"Pilot {FullName} has {NumberOfWins} wins.";
+    }
+}
