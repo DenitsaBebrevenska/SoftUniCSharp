6af4a51be57502235833aa24d1495b92729c486d
Denitsa <denitsabebrevenska@gmail.com>
2024-05-03 00:10:25 +0300

    Readding Sales db

---
+++ EntityFrameworkCore/EntityRelations/Sales/P03_SalesDatabase.Data.Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using P03_SalesDatabase.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P03_SalesDatabase.Data.Models;
+public class Product
+{
+    public Product()
+    {
+        Sales = new HashSet<Sale>();
+    }
+    public int ProductId { get; set; }
+
+    [MaxLength(ValidationConstraints.ProductNameLength)]
+    public string Name { get; set; }
+
+    public decimal Quantity { get; set; }
+
+    public decimal Price { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; }
+}
---
+++ EntityFrameworkCore/EntityRelations/Sales/P03_SalesDatabase.Data.Models/Store.cs
@@ -0,0 +1,17 @@
+﻿using P03_SalesDatabase.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P03_SalesDatabase.Data.Models;
+public class Store
+{
+    public Store()
+    {
+        Sales = new HashSet<Sale>();
+    }
+    public int StoreId { get; set; }
+
+    [MaxLength(ValidationConstraints.StoreNameLength)]
+    public string Name { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; }
+}
