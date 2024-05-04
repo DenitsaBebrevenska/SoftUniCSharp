cdbcc25ca5b41b21ebcb5448595c0fd5ef0ef2df
Denitsa <denitsabebrevenska@gmail.com>
2024-05-04 12:31:53 +0300

    first and second task done

--- EntityFrameworkCore/AdvancedQuerying/BookShop/StartUp.cs
+++ EntityFrameworkCore/AdvancedQuerying/BookShop/StartUp.cs
@@ -1,6 +1,9 @@
 ﻿namespace BookShop;
+
+using BookShop.Models.Enums;
 using Data;
 using Initializer;
+using Microsoft.EntityFrameworkCore;
 
 public class StartUp
 {
@@ -8,6 +11,41 @@ public class StartUp
     {
         using var db = new BookShopContext();
         DbInitializer.ResetDatabase(db);
+
+        //task 02
+        //string? command = Console.ReadLine();
+        //Console.WriteLine(GetBooksByAgeRestriction(db, command.ToLower()));
+
+        //task 03
+        //Console.WriteLine(GetGoldenBooks(db));
+    }
+
+    //task 02
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        AgeRestriction commandEnum = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+        var books = context.Books
+            .AsNoTracking()
+            .Where(b => b.AgeRestriction == commandEnum)
+            .OrderBy(b => b.Title)
+            .Select(b => b.Title)
+            .ToArray();
+
+        return string.Join(Environment.NewLine, books);
+    }
+
+    //task 03
+    public static string GetGoldenBooks(BookShopContext context)
+    {
+        EditionType goldenEdition = EditionType.Gold;
+        var books = context.Books
+            .AsNoTracking()
+            .Where(b => b.EditionType == goldenEdition && b.Copies < 5_000)
+            .OrderBy(b => b.BookId)
+            .Select(b => b.Title)
+            .ToArray();
+
+        return string.Join(Environment.NewLine, books);
     }
 }
 
