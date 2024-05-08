f667bede6a86f5b5a0827dd7d3484b5d712c69d6
Denitsa <denitsabebrevenska@gmail.com>
2024-05-08 21:44:08 +0300

    added task with manual mapping

--- EntityFrameworkCore/JSON/CD/CarDealer/StartUp.cs
+++ EntityFrameworkCore/JSON/CD/CarDealer/StartUp.cs
@@ -13,9 +13,14 @@ namespace CarDealer
             using CarDealerContext context = new CarDealerContext();
 
             //task 09 
-            string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
-            Console.WriteLine(ImportSuppliers(context, inputJson));
+            //string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            //Console.WriteLine(ImportSuppliers(context, inputJson));
 
+            //task 10
+            //string inputJson = File.ReadAllText(@"../../../Datasets/parts.json");
+            //Console.WriteLine(ImportParts(context, inputJson));
+
+            //task 11
         }
 
         private static IMapper CreateMapper()
@@ -24,6 +29,7 @@ namespace CarDealer
                 cfg.AddProfile<CarDealerProfile>()));
         }
 
+        //task 09
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
         {
             var mapper = CreateMapper();
@@ -41,5 +47,36 @@ namespace CarDealer
 
             return $"Successfully imported {validSuppliers.Count}.";
         }
+
+        //task 10
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            //manual mapping because setting a mapper can take longer
+
+            var partDtos = JsonConvert.DeserializeObject<ImportPartDto[]>(inputJson);
+            var partsToImport = new HashSet<Part>();
+
+            foreach (var dto in partDtos)
+            {
+                if (!context.Suppliers.Any(s => s.Id == dto.SupplierId))
+                {
+                    continue;
+                }
+
+                partsToImport.Add(new Part()
+                {
+                    Name = dto.Name,
+                    Price = dto.Price,
+                    Quantity = dto.Quantity,
+                    SupplierId = dto.SupplierId
+                });
+            }
+
+            context.Parts.AddRange(partsToImport);
+            context.SaveChanges();
+            return $"Successfully imported {partsToImport.Count}.";
+        }
+
+        //task 11
     }
 }
