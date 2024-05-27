4b3ba2606401e1f9c8f022f5c7790c21c6967058
Denitsa <denitsabebrevenska@gmail.com>
2024-05-27 23:16:26 +0300

    adding more exercise

--- JSFrontEnd/JSObjectsAndClasses/Exercise/objects-classes-exercise.js
+++ JSFrontEnd/JSObjectsAndClasses/Exercise/objects-classes-exercise.js
@@ -128,31 +128,31 @@ function solve(input) {
 // ]);
 
 //05. Inventory
-function solve(input){
-    class Hero{
-        constructor(name, level, items){
-            this.name = name;
-            this.level = Number(level);
-            this.items = items;
-        }
+function solve(input) {
+  class Hero {
+    constructor(name, level, items) {
+      this.name = name;
+      this.level = Number(level);
+      this.items = items;
     }
+  }
 
-    let heroRegister = [];
+  let heroRegister = [];
 
-    for(let i = 0; i < input.length; i++){
-        let [name, level, items] = input[i].split(' / ');
-        heroRegister.push(new Hero(name, level, items));
-    }
+  for (let i = 0; i < input.length; i++) {
+    let [name, level, items] = input[i].split(" / ");
+    heroRegister.push(new Hero(name, level, items));
+  }
 
-    heroRegister.sort((a, b) => {
-        return a.level - b.level;
-    });
+  heroRegister.sort((a, b) => {
+    return a.level - b.level;
+  });
 
-    heroRegister.forEach(hero =>{
-        console.log(`Hero: ${hero.name}`);
-        console.log(`level => ${hero.level}`);
-        console.log(`items => ${hero.items}`);
-    });
+  heroRegister.forEach((hero) => {
+    console.log(`Hero: ${hero.name}`);
+    console.log(`level => ${hero.level}`);
+    console.log(`items => ${hero.items}`);
+  });
 }
 
 // solve([
@@ -163,46 +163,108 @@ function solve(input){
 
 //06. Word Tracker
 
-function solve(input){
-    let trackedWords = input.shift().split(' ');
-    let wordTracker = {};
+function solve(input) {
+  let trackedWords = input.shift().split(" ");
+  let wordTracker = {};
 
-    trackedWords.forEach(word => wordTracker[word] = 0);
+  trackedWords.forEach((word) => (wordTracker[word] = 0));
 
-    for(const word of input
-        .filter(word => trackedWords.includes(word))){
-            wordTracker[word] += 1;
-    }
+  for (const word of input.filter((word) => trackedWords.includes(word))) {
+    wordTracker[word] += 1;
+  }
 
-    Object.entries(wordTracker)
-        .sort((a,b) => b[1] - a[1])
-        .forEach(([key, value]) => console.log(`${key} - ${value}`));
+  Object.entries(wordTracker)
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([key, value]) => console.log(`${key} - ${value}`));
 }
 
 // solve([
-//     'is the', 
+//     'is the',
 //     'first', 'sentence', 'Here', 'is', 'another', 'the', 'And', 'finally', 'the', 'the', 'sentence']);
 
 //07. Odd Occurrences
-function solve(input){
-    let wordOccurrences = [];
-    let words = input.toLowerCase().split(' ');
-    
-    for(const word of words){
-        let indexWord = wordOccurrences
-            .findIndex(wordObj => wordObj.value === word);
-        
-        if(indexWord < 0){
-            wordOccurrences.push({value: word, count: 1});
-            continue;
-        }
+function solve(input) {
+  let wordOccurrences = [];
+  let words = input.toLowerCase().split(" ");
 
-        wordOccurrences[indexWord].count += 1;
+  for (const word of words) {
+    let indexWord = wordOccurrences.findIndex(
+      (wordObj) => wordObj.value === word
+    );
+
+    if (indexWord < 0) {
+      wordOccurrences.push({ value: word, count: 1 });
+      continue;
     }
 
-    let oddOccurrences = Array.from(wordOccurrences.filter(word => word.count % 2 !== 0)
-        .map(word => word.value));
-    console.log(oddOccurrences.join(' '));   
+    wordOccurrences[indexWord].count += 1;
+  }
+
+  let oddOccurrences = Array.from(
+    wordOccurrences
+      .filter((word) => word.count % 2 !== 0)
+      .map((word) => word.value)
+  );
+  console.log(oddOccurrences.join(" "));
 }
 
 //solve('Cake IS SWEET is Soft CAKE sweet Food');
+
+//08. Piccolo
+function solve(input) {
+  let parkingLot = {};
+
+  for (let i = 0; i < input.length; i++) {
+    let tokens = input[i].split(", ");
+    let direction = tokens[0];
+    let carRegistrationNumber = tokens[1];
+
+    if (direction === "IN") {
+      parkingLot[carRegistrationNumber] = "IN";
+    } else {
+      if (parkingLot.hasOwnProperty(carRegistrationNumber)) {
+        parkingLot[carRegistrationNumber] = "OUT";
+      }
+    }
+  }
+
+  let carsInLot = Object.entries(parkingLot)
+    .filter(([key, value]) => value === "IN")
+    .map(([key, value]) => key)
+    .sort((a, b) => a.localeCompare(b));
+
+  console.log(
+    carsInLot.length === 0 ? "Parking Lot is Empty" : carsInLot.join("\n")
+  );
+}
+
+// solve(['IN, CA2844AA',
+// 'IN, CA1234TA',
+// 'OUT, CA2844AA',
+// 'OUT, CA1234TA']);
+
+//09. Make a Dictionary
+
+function solve(input) {
+  let dictionary = {};
+
+  for (const line of input) {
+    let parsedObject = JSON.parse(line);
+    let [key, value] = Object.entries(parsedObject)[0];
+    dictionary[key] = value;
+  }
+
+  let sortedDictionaryKeys = Object.keys(dictionary).sort();
+
+  sortedDictionaryKeys.forEach((word) =>
+    console.log(`Term: ${word} => Definition: ${dictionary[word]}`)
+  );
+}
+
+solve([
+  '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+  '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+  '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+  '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+  '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
+]);
