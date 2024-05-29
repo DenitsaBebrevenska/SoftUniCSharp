cea65a5feb0b48e09e91ce8019c72e5ef90f62b2
Denitsa <denitsabebrevenska@gmail.com>
2024-05-30 01:16:54 +0300

    adding exercise

--- JSFrontEnd/JSObjectsAndClasses/Exercise/objects-classes-more-exercise.js
+++ JSFrontEnd/JSObjectsAndClasses/Exercise/objects-classes-more-exercise.js
@@ -172,9 +172,9 @@ function solve(input) {
 // ]);
 
 //05. School Register
-function solve(input){
-  class Grade{
-    constructor(number){
+function solve(input) {
+  class Grade {
+    constructor(number) {
       this.number = number;
       this.students = [];
       this.grades = [];
@@ -183,34 +183,40 @@ function solve(input){
 
   let schoolRegister = [];
 
-  for(const line of input){
-    let tokens = line.split(', ')
-      .map(token => token.split(': ')[1]);
+  for (const line of input) {
+    let tokens = line.split(", ").map((token) => token.split(": ")[1]);
     let studentName = tokens[0];
     let studentGrade = Number(tokens[1]);
     let studentAverageScore = Number(tokens[2]);
 
-    if(studentAverageScore < 3){
+    if (studentAverageScore < 3) {
       continue;
     }
 
-    let targetGrade = schoolRegister.find(grade => grade.number == studentGrade + 1);
+    let targetGrade = schoolRegister.find(
+      (grade) => grade.number == studentGrade + 1
+    );
 
-    if(!targetGrade){
+    if (!targetGrade) {
       let grade = new Grade(studentGrade + 1);
       grade.students.push(studentName);
       grade.grades.push(studentAverageScore);
       schoolRegister.push(grade);
-    } else { 
+    } else {
       targetGrade.students.push(studentName);
-      targetGrade.grades.push(studentAverageScore);}
+      targetGrade.grades.push(studentAverageScore);
+    }
   }
 
-  for(const grade of schoolRegister.sort((a, b) => a.number - b.number)){
+  for (const grade of schoolRegister.sort((a, b) => a.number - b.number)) {
     console.log(`${grade.number} Grade`);
-    console.log(`List of students: ${grade.students.join(', ')}`);
-    console.log(`Average annual score from last year: ${(grade.grades.reduce((acc, grade) => acc + grade, 0) / grade.grades.length)
-      .toFixed(2)}\n`);
+    console.log(`List of students: ${grade.students.join(", ")}`);
+    console.log(
+      `Average annual score from last year: ${(
+        grade.grades.reduce((acc, grade) => acc + grade, 0) /
+        grade.grades.length
+      ).toFixed(2)}\n`
+    );
   }
 }
 
@@ -229,36 +235,35 @@ function solve(input){
 //       "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
 //   ]);
 
-
 //06. Browser History
-function solve(browserObj, actions){
+function solve(browserObj, actions) {
   let parsedObj = Object.entries(browserObj);
   let browser = {
     name: parsedObj.shift()[1],
     openTabs: [],
     recentlyClosed: [],
-    logs: []
+    logs: [],
   };
 
   let openedTabs = parsedObj.shift()[1];
-  openedTabs.forEach(site => browser.openTabs.push(site));
+  openedTabs.forEach((site) => browser.openTabs.push(site));
 
   let closedTabs = parsedObj.shift()[1];
-  closedTabs.forEach(site => browser.recentlyClosed.push(site));
+  closedTabs.forEach((site) => browser.recentlyClosed.push(site));
 
   let logs = parsedObj.shift()[1];
-  logs.forEach(log => browser.logs.push(log));
+  logs.forEach((log) => browser.logs.push(log));
 
-  for(const action of actions){
-    if(action.includes('Open')){
-      let siteName = action.split('Open ')[1];
+  for (const action of actions) {
+    if (action.includes("Open")) {
+      let siteName = action.split("Open ")[1];
       browser.openTabs.push(siteName);
       browser.logs.push(action);
-    } else if(action.includes('Close')){
-      let siteName = action.split('Close ')[1];
-      let indexSite = browser.openTabs.findIndex(entry => entry === siteName);
+    } else if (action.includes("Close")) {
+      let siteName = action.split("Close ")[1];
+      let indexSite = browser.openTabs.findIndex((entry) => entry === siteName);
 
-      if(indexSite >= 0){
+      if (indexSite >= 0) {
         browser.openTabs.splice(indexSite, 1);
         browser.logs.push(action);
         browser.recentlyClosed.push(siteName);
@@ -271,12 +276,152 @@ function solve(browserObj, actions){
   }
 
   console.log(browser.name);
-  console.log(`Open Tabs: ${browser.openTabs.join(', ')}`);
-  console.log(`Recently Closed: ${browser.recentlyClosed.join(', ')}`);
-  console.log(`Browser Logs: ${browser.logs.join(', ')}`)
+  console.log(`Open Tabs: ${browser.openTabs.join(", ")}`);
+  console.log(`Recently Closed: ${browser.recentlyClosed.join(", ")}`);
+  console.log(`Browser Logs: ${browser.logs.join(", ")}`);
 }
 
 // solve({"Browser Name":"Google Chrome","Open Tabs":["Facebook","YouTube","Google Translate"],
 // "Recently Closed":["Yahoo","Gmail"],
 // "Browser Logs":["Open YouTube","Open Yahoo","Open Google Translate","Close Yahoo","Open Gmail","Close Gmail","Open Facebook"]},
 // ["Close Facebook", "Open StackOverFlow", "Open Google"]);
+
+//07. Sequences
+
+function solve(numbersArrays) {
+  let arrayLibrary = [];
+
+  for (const line of numbersArrays) {
+    let currentArray = line
+      .slice(1, line.length - 1)
+      .split(", ")
+      .map(Number)
+      .sort((a, b) => b - a);
+
+    if (!arrayLibrary.some((arr) => arr.length === currentArray.length)) {
+      arrayLibrary.push(currentArray);
+      continue;
+    }
+
+    let existingArray = arrayLibrary.find(
+      (arr) =>
+        arr.length === currentArray.length &&
+        arr.toString() === currentArray.toString()
+    );
+
+    if (!existingArray) {
+      arrayLibrary.push(currentArray);
+    }
+  }
+
+  arrayLibrary
+    .sort((a, b) => a.length - b.length)
+    .forEach((arr) => console.log(`[${arr.join(", ")}]`));
+}
+
+// solve(["[7.14, 7.180, 7.339, 80.099]",
+// "[7.339, 80.0990, 7.140000, 7.18]",
+// "[7.339, 7.180, 7.14, 80.099]"]);
+
+//08. Garage
+function solve(input) {
+  let garages = {};
+
+  for (let line of input) {
+    let garageTokens = line.split(" - ");
+    let garageNumber = garageTokens[0];
+
+    if (!garages[garageNumber]) {
+      garages[garageNumber] = [];
+    }
+
+    let carTokens = garageTokens[1].split(", ");
+    let carObj = {};
+    for (const propertyInfo of carTokens) {
+      let [key, value] = propertyInfo.split(": ");
+      carObj[key] = value;
+    }
+
+    garages[garageNumber].push(carObj);
+  }
+
+  for (const garageNumber in garages) {
+    console.log(`Garage № ${garageNumber}`);
+    let cars = garages[garageNumber].map((car) =>
+      Array.from(Object.entries(car))
+    );
+
+    for (const car of cars) {
+      let properties = [];
+      for (const [key, value] of car) {
+        properties.push(`${key} - ${value}`);
+      }
+
+      console.log(`--- ${properties.join(", ")}`);
+    }
+  }
+}
+
+// solve(['1 - color: green, fuel type: petrol',
+// '1 - color: dark red, manufacture: WV',
+// '2 - fuel type: diesel',
+// '3 - color: dark blue, fuel type: petrol']);
+
+//09. Armies
+function solve(input) {
+  let leaders = {};
+
+  for (const line of input) {
+    if (line.includes('arrives')){
+      let leaderName = line.split(' arrives')[0];
+      leaders[leaderName] = [];
+    } else if (line.includes(':')){
+      let lineTokens = line.split(': ');
+      let leaderName = lineTokens[0];
+      
+      if(leaders.hasOwnProperty([leaderName])){
+        let [armyName, count] = lineTokens[1].split(', ');
+        leaders[leaderName].push({
+          armyName,
+          count: Number(count)
+        })
+      }
+    } else if(line.includes('+')){
+      let tokens = line.split(' + ');
+      let armyName = tokens[0];
+      let armyCount = Number(tokens[1]);
+
+      for(const leader in leaders){
+        let armyIndex = leaders[leader].findIndex(army => army.armyName === armyName);
+        if(armyIndex >= 0){
+          leaders[leader][armyIndex].count += armyCount;
+          break;
+        }
+      }
+    } else {
+      let leaderName = line.split(' defeated')[0];
+      if(leaders[leaderName]){
+        delete leaders[leaderName];
+      }
+    }
+  }
+
+  let sortedLeaders = Array.from(Object.entries(leaders));
+  sortedLeaders.sort((a, b) => b[1].lenght - a[1].length); //not sorted 
+}
+
+solve([
+  "Rick Burr arrives",
+  "Fergus: Wexamp, 30245",
+  "Rick Burr: Juard, 50000",
+  "Findlay arrives",
+  "Findlay: Britox, 34540",
+  "Wexamp + 6000",
+  "Juard + 1350",
+  "Britox + 4500",
+  "Porter arrives",
+  "Porter: Legion, 55000",
+  "Legion + 302",
+  "Rick Burr defeated",
+  "Porter: Retix, 3205",
+]);
