35f84241559deef0be2ac716a07d155f628f2801
Denitsa <denitsabebrevenska@gmail.com>
2024-06-01 00:12:01 +0300

    added exercises

--- JSFrontEnd/DOMIntro/Exercise/05. Table - Search Engine/solution.js
+++ JSFrontEnd/DOMIntro/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    let searchWordElement = document.getElementById("searchField");
+    let searchWord = searchWordElement.value;
+    let rowElements = document.querySelectorAll("tbody > tr ");
 
-   }
+    //reset previous searches
+    searchWordElement.value = '';
+    for (const row of rowElements) {
+      row.classList.remove("select");
+    }
+
+    for (const row of rowElements) {
+      if (row.textContent.toLowerCase().includes(searchWord.toLowerCase())) {
+        row.classList.add("select");
+      }
+    }
+  }
 }
--- JSFrontEnd/DOMIntro/Exercise/06. Format the Text/solution.js
+++ JSFrontEnd/DOMIntro/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+  let textAreaElement = document.getElementById('input');
+  let outputContainerElement = document.getElementById('output');
+  let inputText = textAreaElement.value;
+
+  //reset output
+  while(outputContainerElement.firstChild){
+    outputContainerElement.removeChild(outputContainerElement.firstChild);
+  }
+
+  let sentences = inputText.split('.').filter(sentence => {
+    const regex = /[A-Za-z]+/g;
+    return regex.test(sentence);
+  });
+
+  let paragraphText = [];
+  for(let i = 0; i < sentences.length; i++){
+    paragraphText.push(sentences[i]);
+
+    if((i + 1) % 3 === 0 || i === sentences.length - 1){
+      const newParagraph = document.createElement('p');
+      newParagraph.textContent = paragraphText.join('. ') + '.';
+      outputContainerElement.appendChild(newParagraph);
+      paragraphText = []
+    }
+  }
 }
