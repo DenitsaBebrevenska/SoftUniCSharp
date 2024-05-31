a82c26b2ce6145127a25e4a35d05249567291f24
Denitsa <denitsabebrevenska@gmail.com>
2024-05-31 23:01:34 +0300

    added exercise

--- JSFrontEnd/DOMIntro/Exercise/03. Accordion/accordion.js
+++ JSFrontEnd/DOMIntro/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.getElementsByClassName('button')[0];
+    let hiddenTextElement = document.getElementById('extra');
+
+    if(buttonElement.textContent === 'More'){
+        buttonElement.textContent = 'Less';
+        hiddenTextElement.style.display = 'block';
+
+    } else {
+        buttonElement.textContent = 'More';
+        hiddenTextElement.style.display = 'none';
+    }
 }
--- JSFrontEnd/DOMIntro/Exercise/04. Search in List/search.js
+++ JSFrontEnd/DOMIntro/Exercise/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let listItemElements = document.querySelectorAll('li');
+   //reset style upon new search
+   for(const listItem of listItemElements){
+         listItem.style.textDecoration = 'none';
+         listItem.style.fontWeight = 'normal';
+   }
+
+   let searchTextElement = document.getElementById('searchText');
+   let resultElement = document.getElementById('result');
+   let searchWord = searchTextElement.value;
+   let matchesCount = 0;
+   
+   for(const listItem of listItemElements){
+      if(listItem.textContent.toLowerCase().includes(searchWord.toLowerCase())){ //case insensitive could be problematic for Judge
+         listItem.style.textDecoration = 'underline';
+         listItem.style.fontWeight = 'bold';
+         matchesCount++;
+      }
+   }
+
+   resultElement.textContent = `${matchesCount} matches found`;
 }
