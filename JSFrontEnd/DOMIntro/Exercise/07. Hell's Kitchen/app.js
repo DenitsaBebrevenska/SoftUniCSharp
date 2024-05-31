75580216a1d2fa3548a5925f3a0387a8fdb4ca71
Denitsa <denitsabebrevenska@gmail.com>
2024-06-01 01:06:07 +0300

    string split is failing at line 14...will debug tmrw

--- JSFrontEnd/DOMIntro/Exercise/07. Hell's Kitchen/app.js
+++ JSFrontEnd/DOMIntro/Exercise/07. Hell's Kitchen/app.js
@@ -2,7 +2,53 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
-      
+      let inputElement = document.querySelector('#inputs > textarea');
+      let bestRestaurantElement = document.querySelector('#bestRestaurant > p');
+      let bestWorkersElement = document.querySelector('#workers > p');
+      let input = inputElement.value;
+      let restaurants = [];
+
+      for(const line of input){
+         let tokens = line.split(' - ');
+         let restaurantName = tokens[0];
+         let workersInfo = tokens[1].split(', ');
+         let targetedRestaurant = restaurants.find(r => r.name === restaurantName);
+
+         if(!targetedRestaurant){
+            targetedRestaurant = {
+               name: restaurantName,
+               highestSalary: 0,
+               totalSalaryAmount: 0,
+               workers: []
+            }
+            restaurants.push(targetedRestaurant);
+         }
+
+         for(const worker of workersInfo){
+            let tokens = worker.split(' ');
+            let name = tokens[0];
+            let salary = Number(tokens[1]);
+            targetedRestaurant.workers.push({
+               name,
+               salary
+            });
+            targetedRestaurant.totalSalaryAmount += salary;
+
+            if(targetedRestaurant.highestSalary < salary){
+               targetedRestaurant.highestSalary = salary;
+            }
+         }
+      }
+
+      restaurants.sort((a, b) => b.totalSalaryAmount / b.workers.length - a.totalSalaryAmount / a.workers.length);
+      let bestRestaurant = restaurants[0];
+      let averageSalary = bestRestaurant.totalSalaryAmount / bestRestaurant.workers.length;
+      bestRestaurantElement.textContent = 
+         `Name: ${bestRestaurant.name} Average Salary: ${averageSalary.toFixed(2)} Best Salary: ${bestRestaurant.highestSalary.toFixed(2)}`;
+
+      bestRestaurant.workers.sort((a, b) => b.salary - a.salary);
+      let workersResult = bestRestaurant.workers.map(worker => `Name: ${worker.name} With Salary: ${worker.salary}`);
+      bestWorkersElement.textContent = workersResult.join(' ');
    }
 }
+
