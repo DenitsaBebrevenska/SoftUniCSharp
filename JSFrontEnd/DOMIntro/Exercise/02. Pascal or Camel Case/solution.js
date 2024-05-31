a9791265f18675f055c289fcba555ce1f7cc0935
Denitsa <denitsabebrevenska@gmail.com>
2024-05-31 21:19:19 +0300

    exercises added

--- JSFrontEnd/DOMIntro/Exercise/02. Pascal or Camel Case/solution.js
+++ JSFrontEnd/DOMIntro/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  function convertToCamelCase(text){
+    let tokens = text.toLowerCase().split(' ');
+    let result = tokens[0];
+    for(let i = 1; i < tokens.length; i++){
+      result += tokens[i].replace(tokens[i][0], tokens[i][0].toUpperCase());
+    }
+
+    return result;
+  }
+
+  function convertToPascalCase(text){
+    let tokens = text.toLowerCase().split(' ');
+    let result = tokens.map(token => token.replace(token[0],token[0].toUpperCase()))
+    return result.join('');
+  }
+
+  let textContent = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+  let result;
+
+  if(namingConvention === 'Camel Case'){
+    result = convertToCamelCase(textContent);
+  } else if (namingConvention === 'Pascal Case'){
+    result = convertToPascalCase(textContent);
+  } else {
+    result = 'Error!';  
+  }
+
+  document.getElementById('result').textContent = result;
 }
