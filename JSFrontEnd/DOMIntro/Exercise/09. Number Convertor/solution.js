b19c9390fba303c1ceece28bd065287cf2486293
Denitsa <denitsabebrevenska@gmail.com>
2024-06-01 16:26:51 +0300

    last exercises works perefect in browser, not in Judge...

--- JSFrontEnd/DOMIntro/Exercise/09. Number Convertor/solution.js
+++ JSFrontEnd/DOMIntro/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,52 @@
 function solve() {
+    function convertTOBinary(number){
+        let result = '';
+        while(number / 2 !== 0){
+            result = result.concat(number % 2);
+            number = Math.trunc(number / 2);
+        }
+        return result.split('')
+        .reverse()
+        .join('');
+    }
 
-    //TODO...
+    function convertToHexadecimal(number){
+        let result = '';
+        while(number / 16 !== 0){
+            result = result.concat(number % 16 + ' ');
+            number = Math.trunc(number / 16);    
+        }
+
+        result = result.split(' ').map(char =>{
+            if(char === '10'){
+                char ='A'
+            } else if(char === '11'){
+                char = 'B'
+            } else if(char === '12'){
+                char = 'C'
+            } else if(char === '13'){
+                char = 'D'
+            } else if(char === '14'){
+                char = 'E'
+            } else if(char === '15'){
+                char = 'F'
+            } 
+            return char
+        })
+
+        return result.reverse().join('');
+    }
+
+    let numberElementValue = document.getElementById('input').value;
+    let toMenuElementValue = document.getElementById('selectMenuTo').value;
+    let resultElement = document.getElementById('result');
+    let result = '';
+
+    if(toMenuElementValue === 'binary'){
+        result = convertTOBinary(numberElementValue)
+    } else if (toMenuElementValue === 'hexadecimal'){
+        result = convertToHexadecimal(numberElementValue)
+    }
+   
+    resultElement.value = result;
 }
