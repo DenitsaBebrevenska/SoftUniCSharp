cb69d55f8ea5d9b9de919803d129dac305f37b4b
Denitsa <denitsabebrevenska@gmail.com>
2024-06-03 18:06:17 +0300

    added lab exercises

--- JSFrontEnd/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
+++ JSFrontEnd/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+    let listItemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    //create li item
+    let newListItem = document.createElement('li');
+    newListItem.textContent = inputElement.value;
+
+    //create anchor
+    let anchorItem = document.createElement('a');
+    anchorItem.setAttribute('href', '#');
+    anchorItem.textContent = '[Delete]';
+
+    //register event listener on clicking anchor tag
+    anchorItem.addEventListener('click', function(){
+        listItemsElement.removeChild(newListItem);
+    })
+
+    //append children
+    newListItem.appendChild(anchorItem);
+    listItemsElement.appendChild(newListItem);
+
+    //reset input
+    inputElement.value = '';
 }
--- JSFrontEnd/DOMManipulationAndEvents/Lab/04. Mouse-Gradient/app.js
+++ JSFrontEnd/DOMManipulationAndEvents/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,10 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', function(event){
+        let widthElement = event.target.clientWidth;
+        let currentPosition = event.offsetX;
+        resultElement.textContent = `${Math.floor(currentPosition / widthElement * 100)}%`;
+    })
 }
