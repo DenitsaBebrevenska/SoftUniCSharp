a3787f38a80df471f44651ae214e45080bc25ad0
Denitsa <denitsabebrevenska@gmail.com>
2024-06-03 17:05:22 +0300

    lab exercises added

--- JSFrontEnd/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
+++ JSFrontEnd/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,8 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemsMenuElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+    let newListItem = document.createElement('li');
+    newListItem.textContent = inputElement.value;
+    itemsMenuElement.appendChild(newListItem);
+    inputElement.value = '';
 }
--- JSFrontEnd/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
+++ JSFrontEnd/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,11 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let tbodyElement = document.querySelector('table[id="customers"] tbody');
+    let trElements = Array.from(document.querySelectorAll('tbody tr'));
+    let inputElement = document.querySelector('label input[type="text"]');
+    let resultElement = document.getElementById('result');
+    let elementsToRemove = trElements
+        .filter(element => element.textContent.includes(inputElement.value));
+    resultElement.textContent = elementsToRemove.length === 0 ? 'Not found.' : 'Deleted.';
+    console.log(resultElement.textContent);
+    elementsToRemove.forEach(element => tbodyElement.removeChild(element));
 }
