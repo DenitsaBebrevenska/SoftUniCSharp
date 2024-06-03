5e2988d2a034b158c9f043c6bdc60e4a78db2da9
Denitsa <denitsabebrevenska@gmail.com>
2024-06-04 00:54:03 +0300

    added exercises

--- JSFrontEnd/DOMManipulationAndEvents/Exercise/01. Sections/script.js
+++ JSFrontEnd/DOMManipulationAndEvents/Exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentContainerElement = document.getElementById('content');
+   words.forEach(word => {
+      let innerDiv = document.createElement('div');
+      let paragraph = document.createElement('p')
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+      innerDiv.appendChild(paragraph);
+      contentContainerElement.appendChild(innerDiv);
+      
+      innerDiv.addEventListener('click', function(){
+         paragraph.style.display = 'block';
+      })
+   })
 }
--- JSFrontEnd/DOMManipulationAndEvents/Exercise/02. Time-Converter/solution.js
+++ JSFrontEnd/DOMManipulationAndEvents/Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,47 @@
 function attachEventsListeners() {
+  //days
+  let dayInputElement = document.getElementById("days");
+  let dayConvertButton = document.getElementById("daysBtn");
 
-    console.log('TODO:...');
+  //hours
+  let hourInputElement = document.getElementById("hours");
+  let hourConvertButton = document.getElementById("hoursBtn");
+
+  //minutes
+  let minuteInputElement = document.getElementById("minutes");
+  let minuteConvertButton = document.getElementById("minutesBtn");
+
+  //seconds
+  let secondInputElement = document.getElementById("seconds");
+  let secondConvertButton = document.getElementById("secondsBtn");
+
+  //click event for days
+    dayConvertButton.addEventListener('click', function(){
+        let dayValue = Number(dayInputElement.value);
+        hourInputElement.value = dayValue * 24;
+        minuteInputElement.value = dayValue * 1440;
+        secondInputElement.value = dayValue * 86400;
+    })
+
+  //click event for hours
+  hourConvertButton.addEventListener('click', function(){
+    let hourValue = Number(hourInputElement.value);
+    dayInputElement.value = hourValue / 24;
+    minuteInputElement.value = hourValue * 60;
+    secondInputElement.value = hourValue * 3600;
+  })
+  //click even for minutes
+  minuteConvertButton.addEventListener('click', function(){
+    let minuteValue = Number(minuteInputElement.value);
+    dayInputElement.value = minuteValue / 1440;
+    hourInputElement.value = minuteValue / 60;
+    secondInputElement.value = minuteValue * 60;
+  })
+  //click even for seconds
+  secondConvertButton.addEventListener('click', function(){
+    let secondValue = Number(secondInputElement.value);
+    dayInputElement.value = secondValue / 86400;
+    hourInputElement.value = secondValue / 3600;
+    minuteInputElement.value = secondValue / 60;
+  })
 }
