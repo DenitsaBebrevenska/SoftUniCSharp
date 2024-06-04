bd1d0dddb0cbedffebaa73c386868d6d71d7b034
Denitsa <denitsabebrevenska@gmail.com>
2024-06-04 18:18:37 +0300

    adding exercise 5th

--- JSFrontEnd/DOMManipulationAndEvents/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JSFrontEnd/DOMManipulationAndEvents/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,29 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let decodedMessageElement = document.querySelector('#main > div:first-child > textarea');
+    let encodedMessageElement = document.querySelector('#main > div:last-child > textarea');
+    let decodingButton = document.querySelector('#main > div:first-child > button');
+    let encodingButton = document.querySelector('#main > div:last-child > button');
+    
+    const encodeMessage = function(text){
+       let asciiValues = text.split('')
+            .map(char => char.charCodeAt(0) + 1);
+        return String.fromCharCode(...asciiValues);
+    }
+
+    const decodeMessage = function(text){
+        let asciiValues = text.split('')
+            .map(char => char.charCodeAt(0) - 1);
+        return String.fromCharCode(...asciiValues);
+    }
+
+    decodingButton.addEventListener('click', function(){
+        let text = encodeMessage(decodedMessageElement.value);
+        encodedMessageElement.value = text;
+        decodedMessageElement.value= '';
+    })
+
+    encodingButton.addEventListener('click', function(){
+        let text = decodeMessage(encodedMessageElement.value);
+        encodedMessageElement.value = text;
+    })
 }
