e5cf76d8099961636d677186a29a9242066b5373
Denitsa <denitsabebrevenska@gmail.com>
2024-06-05 14:46:07 +0300

    distance converter added

--- JSFrontEnd/DOMManipulationAndEvents/Exercise/07. Distance-Converter/distanceConverter.js
+++ JSFrontEnd/DOMManipulationAndEvents/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,27 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('inputDistance');
+    let inputUnitElement = document.getElementById('inputUnits');
+    let outputElement = document.getElementById('outputDistance');
+    let outputUnitElement = document.getElementById('outputUnits');
+
+    const unitsToMeterMap = new Map([
+        ['km', 1000],
+        ['m', 1],
+        ['cm', 0.01],
+        ['mm', 0.001],
+        ['mi', 1609.34],
+        ['yrd', 0.9144],
+        ['ft', 0.3048],
+        ['in', 0.0254]
+    ]);
+
+    let convertButton = document.querySelector('input[type="button"]');
+    convertButton.addEventListener('click', function(){
+        let inputValue = Number(inputElement.value);
+        let inputUnit = inputUnitElement.value;
+        let inputToMeters = unitsToMeterMap.get(inputUnit) * inputValue;
+        let outputUnit = outputUnitElement.value;
+        outputElement.value = inputToMeters / unitsToMeterMap.get(outputUnit);
+    })
+
 }
