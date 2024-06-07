aaf238712c18cd89aa52c0b2dea84c732d16d6c6
Denitsa <denitsabebrevenska@gmail.com>
2024-06-08 01:28:18 +0300

    more labs done but not passing 6th test yet, goes into timeout, there must be something yet to find unhandled

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/02.Bus-Schedule/app.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/02.Bus-Schedule/app.js
@@ -1,10 +1,36 @@
 function solve() {
+    let infoTextElement = document.getElementById('info');
+    let departButtonElement = document.getElementById('depart');
+    let arrivalButtonElement = document.getElementById('arrive');
+    let url = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let currentStop = '';
+    let nextStop = 'depot';
+
     function depart() {
-        // TODO: 
+       fetch(url + nextStop)
+       .then((response) => {
+        if(response.ok){
+           return response.json()}
+           throw new Error;
+    })
+       .then((busStopData) => {
+        currentStop = busStopData.name;
+        infoTextElement.textContent = `Next stop ${currentStop}`;
+        departButtonElement.disabled = true;
+        arrivalButtonElement.disabled = false;
+        nextStop = busStopData.next;
+       })
+       .catch(err => {
+        infoTextElement.textContent = 'Error';
+        departButtonElement.disabled = true;
+        arrivalButtonElement.disabled = true;
+       })
     }
 
     async function arrive() {
-        // TODO:
+        infoTextElement.textContent = `Arriving at ${currentStop}`;
+        departButtonElement.disabled = false;
+        arrivalButtonElement.disabled = true;
     }
 
     return {
