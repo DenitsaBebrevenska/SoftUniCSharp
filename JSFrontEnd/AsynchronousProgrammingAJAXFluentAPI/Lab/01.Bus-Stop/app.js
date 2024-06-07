d51a65a5579dc7be72c6d7c9220cba0e99a9be42
Denitsa <denitsabebrevenska@gmail.com>
2024-06-08 00:56:16 +0300

    more lab exercise added

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/01.Bus-Stop/app.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    // TODO:
+    const url = 'http://localhost:3030/jsonstore/bus/businfo/';
+    let inputStopElement = document.getElementById('stopId');
+    let stopNameResultElement = document.getElementById('stopName');
+    let bussesListElement = document.getElementById('buses');
+    let targetBusStop = inputStopElement.value;
+
+    bussesListElement.innerHTML = '';
+    
+    fetch(url + targetBusStop)
+    .then((response) => response.json())
+    .then((data) => {
+        stopNameResultElement.textContent = data.name;
+        let fragment = document.createDocumentFragment();
+
+        for (const bus in data.buses) {
+           let newLiElement = document.createElement('li');
+           newLiElement.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
+           fragment.appendChild(newLiElement);
+        }
+        bussesListElement.appendChild(fragment);
+    })
+    .catch(err => stopNameResultElement.textContent = `Error`)
+
 }
