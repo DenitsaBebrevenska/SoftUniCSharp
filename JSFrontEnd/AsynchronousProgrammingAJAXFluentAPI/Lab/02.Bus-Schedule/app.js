68213659ed9786180ff66d83964d599621c67001
Denitsa <denitsabebrevenska@gmail.com>
2024-06-08 13:48:11 +0300

    all along the problem was the wrong selected input text element

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/02.Bus-Schedule/app.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Lab/02.Bus-Schedule/app.js
@@ -1,42 +1,56 @@
 function solve() {
-    let infoTextElement = document.getElementById('info');
-    let departButtonElement = document.getElementById('depart');
-    let arrivalButtonElement = document.getElementById('arrive');
-    let url = 'http://localhost:3030/jsonstore/bus/schedule/';
-    let currentStop = '';
-    let nextStop = 'depot';
+  let infoTextElement = document.querySelector("#info > span");
+  let departButtonElement = document.getElementById("depart");
+  let arrivalButtonElement = document.getElementById("arrive");
+  let url = "http://localhost:3030/jsonstore/bus/schedule/";
+  let currentStop = "";
+  let nextStop = "depot";
 
-    function depart() {
-       fetch(url + nextStop)
-       .then((response) => {
-        if(response.ok){
-           return response.json()}
-           throw new Error;
-    })
-       .then((busStopData) => {
-        currentStop = busStopData.name;
-        infoTextElement.textContent = `Next stop ${currentStop}`;
-        departButtonElement.disabled = true;
-        arrivalButtonElement.disabled = false;
-        nextStop = busStopData.next;
-       })
-       .catch(err => {
-        infoTextElement.textContent = 'Error';
-        departButtonElement.disabled = true;
-        arrivalButtonElement.disabled = true;
-       })
+  // first solution before knowing async await syntax
+  //   function depart() {
+  //     fetch(url + nextStop)
+  //     .then((response) => response.json())
+  //     .then((busStopData) => {
+  //      currentStop = busStopData.name;
+  //      infoTextElement.textContent = `Next stop ${currentStop}`;
+  //      departButtonElement.disabled = true;
+  //      arrivalButtonElement.disabled = false;
+  //      nextStop = busStopData.next;
+  //     })
+  //     .catch(err => {
+  //      infoTextElement.textContent = 'Error';
+  //      departButtonElement.disabled = true;
+  //      arrivalButtonElement.disabled = true;
+  //     })
+  //  }
+
+  //second solution
+  async function depart() {
+    try {
+      const response = await fetch(url + nextStop);
+      const data = await response.json();
+
+      currentStop = data.name;
+      infoTextElement.textContent = `Next stop ${currentStop}`;
+      departButtonElement.disabled = true;
+      arrivalButtonElement.disabled = false;
+      nextStop = data.next;
+    } catch {
+      infoTextElement.textContent = "Error";
+      departButtonElement.disabled = true;
     }
+  }
 
-    async function arrive() {
-        infoTextElement.textContent = `Arriving at ${currentStop}`;
-        departButtonElement.disabled = false;
-        arrivalButtonElement.disabled = true;
-    }
+  async function arrive() {
+    infoTextElement.textContent = `Arriving at ${currentStop}`;
+    departButtonElement.disabled = false;
+    arrivalButtonElement.disabled = true;
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
