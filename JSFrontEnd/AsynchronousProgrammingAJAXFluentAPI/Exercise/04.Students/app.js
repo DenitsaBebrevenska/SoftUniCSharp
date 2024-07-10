264c9aa88618a0ab6c549e49a316734f4800aeba
Denitsa <denitsabebrevenska@gmail.com>
2024-07-10 23:37:48 +0300

    all completed but the grade rounding

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/04.Students/app.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/04.Students/app.js
@@ -37,9 +37,51 @@ function attachEvents() {
   let inputFacultyNumberElement = document.querySelector('input[name=facultyNumber]');
   let inputGradeElement = document.querySelector('input[name=grade]');
 
-  fetch(url)
-  .then((postResponse) => postResponse.json())
-  .then()
+  submitButtonElement.addEventListener('click', async function(){
+    try{
+      let newEntry = JSON.stringify({
+        firstName: inputFirstNameElement.value,
+        lastName: inputLastNameElement.value,
+        facultyNumber: inputFacultyNumberElement.value,
+        grade: Number(inputGradeElement.value)
+      });
+
+      let tdFirstName = document.createElement('td');
+      let tdLastName = document.createElement('td');
+      let tdNumber = document.createElement('td');
+      let tdGrade = document.createElement('td');
+
+      tdFirstName.textContent = inputFirstNameElement.value;
+      tdLastName.textContent = inputLastNameElement.value;
+      tdNumber.textContent = inputFacultyNumberElement.value;
+      tdGrade.textContent = Number(inputGradeElement.value);
+
+      let trElement = document.createElement('tr');
+      trElement.appendChild(tdFirstName);
+      trElement.appendChild(tdLastName);
+      trElement.appendChild(tdNumber);
+      trElement.appendChild(tdGrade);
+
+      tableBodyElement.appendChild(trElement);
+
+      inputFirstNameElement.value = "";
+      inputLastNameElement.value = "";
+      inputFacultyNumberElement.value = "";
+      inputGradeElement.value = "";
+
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: newEntry,
+      };
+
+      const postResponse = await fetch(url, requestOptions);
+      const postData = await postResponse.json();
+    } catch (error){
+      console.log(error);
+    }})
 }
 
 attachEvents();
