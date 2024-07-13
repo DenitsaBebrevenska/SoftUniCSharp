1fb5900758e475569cba21956607f7bf9ff73abb
Denitsa <denitsabebrevenska@gmail.com>
2024-07-13 18:19:08 +0300

    adding login page script

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/07. Fisher-Game/src/login.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/07. Fisher-Game/src/login.js
@@ -1,1 +1,11 @@
-console.log('TODO:// Implement Login functionality');
+async function logIn(){
+    //highlight the current element in use
+    let homeElement = document.getElementById('home');
+    homeElement.classList.remove('active');
+    let loginElement = document.getElementById('login');
+    loginElement.classList.add('active');
+
+    const logInUrl = 'http://localhost:3030/users/login';
+}
+
+logIn();
