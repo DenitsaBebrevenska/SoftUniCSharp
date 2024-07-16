f95daa3a0ed5f0bdc9755554ad1c5d7f60ee88b2
Denitsa <denitsabebrevenska@gmail.com>
2024-07-17 00:31:09 +0300

    implemented some load btn functionality

--- JSFrontEnd/ExamPrep/RetakeExam19April2024/02.AdoptMe/app.js
+++ JSFrontEnd/ExamPrep/RetakeExam19April2024/02.AdoptMe/app.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO..
+    let inputTypeElement = document.getElementById('type');
+    let inputAgeElement = document.getElementById('age');
+    let inputGenderElement = document.getElementById('gender');
+    let adoptButttonElement = document.getElementById('adopt-btn');
+    let adoptionInfoListElement = document.getElementById('adoption-info');
+    let adoptedListElement = document.getElementById('adopted-list');
   }
   
