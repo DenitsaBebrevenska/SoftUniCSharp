8b2e1e9325be10d5b1196a4144a012b50149dd42
Denitsa <denitsabebrevenska@gmail.com>
2024-07-17 00:34:28 +0300

    added some code to the 2nd taks

--- JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/07. Fisher-Game/src/app.js
+++ JSFrontEnd/AsynchronousProgrammingAJAXFluentAPI/Exercise/07. Fisher-Game/src/app.js
@@ -1,51 +1,60 @@
+//hide catches and add text plus add click event to load btn
+let catchesElement = document.getElementById("catches");
+catchesElement.style.display = "none";
+let legendElement = document.querySelector("legend");
+legendElement.style.display = "none";
+let mainElement = document.getElementById("main");
+mainElement.textContent = "Click to load catches";
+mainElement.style.border = "none";
+let loadBtnElement = document.querySelector("button.load");
+
+loadBtnElement.addEventListener("click", async function () {
+    //display hidden elements
+    legendElement.style.display = "block";
+    catchesElement.style.display = "inline-block";
+    mainElement.textContent = "";
+    mainElement.style.border = "2px solid black";
+});
+
 //if user is logged in pressing logout will log them out
-let token = localStorage.getItem('userToken');
-let logoutBtnElement = document.getElementById('logout');
-let usernameElement = document.querySelector('p.email > span');
+let token = localStorage.getItem("userToken");
+let logoutBtnElement = document.getElementById("logout");
+let usernameElement = document.querySelector("p.email > span");
 
-if (token){
-    //hide inappropriate buttons register and login for logged in users
-    document.getElementById('register').style.display = 'none';
-    document.getElementById('login').style.display = 'none';
-    //set username in welcoming message
-    let username = localStorage.getItem('userEmail');
-    usernameElement.textContent = username.slice(1, username.length - 1);
-    //add event listener for logout button
-    logoutBtnElement.addEventListener('click', async function(){
-        let logoutUrl = 'http://localhost:3030/users/logout';
-        let userToken = localStorage.getItem('userToken');
-        try {
-            await fetch(logoutUrl, {
-                method: 'GET',
-                headers:{
-                    'X-Authorization': userToken
-                }
-            })
-        } catch (error){
-            console.error('Logout failed:', error);
-        }
-        
-        localStorage.removeItem('userToken');
-        localStorage.removeItem('userEmail');
-        usernameElement.textContent = 'guest';
-        window.location.href = 'index.html';
-    })
+if (token) {
+  //hide inappropriate buttons register and login for logged in users
+  document.getElementById("register").style.display = "none";
+  document.getElementById("login").style.display = "none";
+  //set username in welcoming message
+  let username = localStorage.getItem("userEmail");
+  usernameElement.textContent = username.slice(1, username.length - 1);
+  //add event listener for logout button
+  logoutBtnElement.addEventListener("click", async function () {
+    let logoutUrl = "http://localhost:3030/users/logout";
+    let userToken = localStorage.getItem("userToken");
+    try {
+      await fetch(logoutUrl, {
+        method: "GET",
+        headers: {
+          "X-Authorization": userToken,
+        },
+      });
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+
+    localStorage.removeItem("userToken");
+    localStorage.removeItem("userEmail");
+    usernameElement.textContent = "guest";
+    window.location.href = "index.html";
+  });
 } else {
-    logoutBtnElement.style.display = 'none';
-    //disable all buttons a guest user cannot click 
-    document.querySelectorAll('.update').forEach(button => {
-        button.disabled = 'true';
-    })
-    document.querySelectorAll('.delete').forEach(button => {
-        button.disabled = 'true';
-    })
-    //hide catches and add text plus add click event to load btn
-    document.getElementById('catches').style.display = 'none';
-    document.querySelector('legend').style.display = 'none';
-    let mainElement = document.getElementById('main');
-    mainElement.textContent = 'Click to load catches';
-    mainElement.style.border = 'none';
-
-
+  logoutBtnElement.style.display = "none";
+  //disable all buttons a guest user cannot click
+  document.querySelectorAll(".update").forEach((button) => {
+    button.disabled = "true";
+  });
+  document.querySelectorAll(".delete").forEach((button) => {
+    button.disabled = "true";
+  });
 }
-//welcome guest, guest be replaced by username
