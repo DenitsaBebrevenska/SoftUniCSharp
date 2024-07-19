064432633f9de8376286a140c3e0097fde6433d1
Denitsa <denitsabebrevenska@gmail.com>
2024-07-19 23:23:18 +0300

    implemented adopt button functionality

--- JSFrontEnd/ExamPrep/RetakeExam19April2024/02.AdoptMe/app.js
+++ JSFrontEnd/ExamPrep/RetakeExam19April2024/02.AdoptMe/app.js
@@ -1,11 +1,50 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    let inputTypeElement = document.getElementById('type');
-    let inputAgeElement = document.getElementById('age');
-    let inputGenderElement = document.getElementById('gender');
-    let adoptButttonElement = document.getElementById('adopt-btn');
-    let adoptionInfoListElement = document.getElementById('adoption-info');
-    let adoptedListElement = document.getElementById('adopted-list');
+  //target needed elements 
+  let typeInputElement = document.getElementById('type');
+  let ageInputElement = document.getElementById('age');
+  let genderInputElement = document.getElementById('gender');
+  let adoptBtnElement = document.getElementById('adopt-btn');
+  let adoptionInfoElement = document.getElementById('adoption-info');
+  //add click event
+  adoptBtnElement.addEventListener('click', function(event){
+    event.preventDefault();
+    //all fields must have input
+    if(typeInputElement.value.length > 0 &&
+       ageInputElement.value.length > 0 &&
+       genderInputElement.value.length > 0){
+        //create all needed elements and append text and classes accordingly and append children
+        let newLiElement = document.createElement('li');
+        let newArticleElement = document.createElement('article');
+        let petTypeElement = document.createElement('p');
+        let petGenderElement = document.createElement('p');
+        let petAgeElement = document.createElement('p');
+        petTypeElement.textContent = `Pet: ${typeInputElement.value}`;
+        petGenderElement.textContent = `Gender: ${genderInputElement.value}`;
+        petAgeElement.textContent = `Age: ${ageInputElement.value}`;
+        newArticleElement.appendChild(petTypeElement);
+        newArticleElement.appendChild(petGenderElement);
+        newArticleElement.appendChild(petAgeElement);
+        let divBtnElement = document.createElement('div');
+        divBtnElement.classList.add('buttons');
+        let editBtnElement = document.createElement('button');
+        editBtnElement.classList.add('edit-btn');
+        editBtnElement.textContent = 'Edit';
+        let doneBtnElement = document.createElement('button');
+        doneBtnElement.classList.add('done-btn');
+        doneBtnElement.textContent = 'Done';
+        divBtnElement.appendChild(editBtnElement);
+        divBtnElement.appendChild(doneBtnElement);
+        newLiElement.appendChild(newArticleElement);
+        newLiElement.appendChild(divBtnElement);
+        //append new animal the adoption info
+        adoptionInfoElement.appendChild(newLiElement);
+        //clear input fields
+        petTypeElement.value = '';
+        petGenderElement.value = '';
+        petAgeElement.value = '';
+       }
+  })
   }
   
