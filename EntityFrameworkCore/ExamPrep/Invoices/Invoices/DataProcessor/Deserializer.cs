db6b4ee5987fd74f9f8263161ac344e82e4777bb
Denitsa <denitsabebrevenska@gmail.com>
2024-07-24 00:46:02 +0300

    import dtos added and ImportInvoices implemented

--- EntityFrameworkCore/ExamPrep/Invoices/Invoices/DataProcessor/Deserializer.cs
+++ EntityFrameworkCore/ExamPrep/Invoices/Invoices/DataProcessor/Deserializer.cs
@@ -1,6 +1,9 @@
 ﻿using Invoices.Common;
 using Invoices.Data.Models;
+using Invoices.Data.Models.Enums;
 using Invoices.DataProcessor.ImportDto;
+using Newtonsoft.Json;
+using System.Globalization;
 using System.Text;
 
 namespace Invoices.DataProcessor
@@ -75,7 +78,49 @@ namespace Invoices.DataProcessor
 
         public static string ImportInvoices(InvoicesContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var invoiceDtos = JsonConvert.DeserializeObject<ImportInvoiceDto[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+            var validInvoices = new HashSet<Invoice>();
+            Client[] clients = context.Clients.ToArray();
+
+            foreach (var invoiceDto in invoiceDtos)
+            {
+                DateTime issueDateResult;
+                DateTime dueDateResult;
+                CurrencyType currencyTypeResult;
+
+                //if invalid issue or due date, due date is before issue date, invalid amount, currency type or client, continue
+                if (!IsValid(invoiceDto) ||
+                    clients.All(c => c.Id != invoiceDto.ClientId) ||
+                    !DateTime.TryParse(invoiceDto.DueDate, CultureInfo.InvariantCulture, DateTimeStyles.None, out dueDateResult) ||
+                    !DateTime.TryParse(invoiceDto.IssueDate, CultureInfo.InvariantCulture, DateTimeStyles.None, out issueDateResult) ||
+                    dueDateResult < issueDateResult ||
+                    (invoiceDto.CurrencyType > (int)CurrencyType.USD || invoiceDto.CurrencyType < (int)CurrencyType.BGN))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Invoice invoice = new Invoice()
+                {
+                    Number = invoiceDto.Number,
+                    IssueDate = issueDateResult,
+                    DueDate = dueDateResult,
+                    Amount = invoiceDto.Amount,
+                    CurrencyType = (CurrencyType)invoiceDto.CurrencyType
+                };
+
+                Client client = clients.First(c => c.Id == invoiceDto.ClientId);
+                client.Invoices.Add(invoice);
+                invoice.Client = client;
+                validInvoices.Add(invoice);
+                sb.AppendLine(string.Format(SuccessfullyImportedInvoices, invoice.Number));
+            }
+
+            context.Invoices.AddRange(validInvoices);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportProducts(InvoicesContext context, string jsonString)
---
+++ EntityFrameworkCore/ExamPrep/Invoices/Invoices/DataProcessor/ImportDto/ImportInvoiceDto.cs
@@ -0,0 +1,25 @@
+﻿using Invoices.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.DataProcessor.ImportDto;
+public class ImportInvoiceDto
+{
+    [Required]
+    [Range(TableConstraints.InvoiceMinNumber, TableConstraints.InvoiceMaxNumber)]
+    public int Number { get; set; }
+
+    [Required]
+    public string IssueDate { get; set; }
+
+    [Required]
+    public string DueDate { get; set; }
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public int CurrencyType { get; set; }
+
+    [Required]
+    public int ClientId { get; set; }
+}
