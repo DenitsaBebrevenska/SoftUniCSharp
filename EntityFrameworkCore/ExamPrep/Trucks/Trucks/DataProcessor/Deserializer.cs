ee475c612dfc4440ea64e573702277729a28a221
Denitsa <denitsabebrevenska@gmail.com>
2024-07-29 18:16:23 +0300

    added xml helper deserialize and ImportDespatcher implemented

--- EntityFrameworkCore/ExamPrep/Trucks/Trucks/DataProcessor/Deserializer.cs
+++ EntityFrameworkCore/ExamPrep/Trucks/Trucks/DataProcessor/Deserializer.cs
@@ -1,4 +1,10 @@
-﻿namespace Trucks.DataProcessor
+﻿using System.Text;
+using Trucks.Common;
+using Trucks.Data.Models;
+using Trucks.Data.Models.Enums;
+using Trucks.DataProcessor.ImportDto;
+
+namespace Trucks.DataProcessor
 {
     using Data;
     using System.ComponentModel.DataAnnotations;
@@ -16,11 +22,68 @@
 
         public static string ImportDespatcher(TrucksContext context, string xmlString)
         {
+            string rootName = "Despatchers";
+            var despatcherDtos = XmlHelper.Deserialize<ImportDespatcherDto[]>(xmlString, rootName);
+            var validDespatchers = new HashSet<Despatcher>();
+            StringBuilder sb = new StringBuilder();
 
+            foreach (var despatcherDto in despatcherDtos)
+            {
+                if (!IsValid(despatcherDto) ||
+                    string.IsNullOrEmpty(despatcherDto.Position)) //or whitespace?
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Despatcher despatcher = new Despatcher()
+                {
+                    Name = despatcherDto.Name,
+                    Position = despatcherDto.Position
+                };
+
+                foreach (var truckDto in despatcherDto.Trucks)
+                {
+                    int categoryTypeMinValue = Enum.GetValues(typeof(CategoryType)).Cast<int>().Min();
+                    int categoryTypeMaxValue = Enum.GetValues(typeof(CategoryType)).Cast<int>().Max();
+                    int makeTypeMinValue = Enum.GetValues(typeof(MakeType)).Cast<int>().Min();
+                    int makeTypeMaxValue = Enum.GetValues(typeof(MakeType)).Cast<int>().Max();
+
+                    if (!IsValid(truckDto) ||
+                        (truckDto.CategoryType < categoryTypeMinValue ||
+                         truckDto.CategoryType > categoryTypeMaxValue) ||
+                        (truckDto.MakeType < makeTypeMinValue ||
+                         truckDto.MakeType > makeTypeMaxValue))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Truck truck = new Truck()
+                    {
+                        RegistrationNumber = truckDto.RegistrationNumber,
+                        VinNumber = truckDto.VinNumber,
+                        TankCapacity = truckDto.TankCapacity,
+                        CargoCapacity = truckDto.CargoCapacity,
+                        CategoryType = (CategoryType)truckDto.CategoryType,
+                        MakeType = (MakeType)truckDto.MakeType,
+                        Despatcher = despatcher
+                    };
+
+                    despatcher.Trucks.Add(truck);
+                }
+
+                validDespatchers.Add(despatcher);
+                sb.AppendLine(string.Format(SuccessfullyImportedDespatcher, despatcher.Name, despatcher.Trucks.Count));
+            }
+
+            context.Despatchers.AddRange(validDespatchers);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
         public static string ImportClient(TrucksContext context, string jsonString)
         {
-            throw new NotImplementedException();
+
         }
 
         private static bool IsValid(object dto)
---
+++ EntityFrameworkCore/ExamPrep/Trucks/Trucks/DataProcessor/ImportDto/ImportClientDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Common;
+
+namespace Trucks.DataProcessor.ImportDto;
+public class ImportClientDto
+{
+    [Required]
+    [StringLength(TableConstraints.ClientNameMaxLength, MinimumLength = TableConstraints.ClientNameMinLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [StringLength(TableConstraints.ClientNationalityMaxLength, MinimumLength = TableConstraints.ClientNationalityMinLength)]
+    public string Nationality { get; set; } = null!;
+
+    [Required]
+    public string Type { get; set; } = null!;
+
+    public int[] Trucks { get; set; }
+}
