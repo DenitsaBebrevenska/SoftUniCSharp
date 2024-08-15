57ed31e5002770ba249a33527b2ed7c3eceffdff
Denitsa <denitsabebrevenska@gmail.com>
2024-08-15 14:28:36 +0300

    add present functionality implemented

--- JSFrontEnd/ExamPrep/RetakeExam22December2023/03.ChristmasGifts/app.js
+++ JSFrontEnd/ExamPrep/RetakeExam22December2023/03.ChristmasGifts/app.js
@@ -4,12 +4,58 @@ let giftListElement = document.getElementById("gift-list");
 let sampleGiftItemElement = document.querySelector(
   "#gift-list > div.gift-sock"
 );
+let formElement = document.querySelector("#form > form");
+let giftInputElement = document.getElementById("gift");
+let forInputElement = document.getElementById("for");
+let priceInputElement = document.getElementById("price");
+let addPresentBtnElement = document.getElementById("add-present");
 
 //clear up sample element
 giftListElement.innerHTML = "";
 
 //load btn functionality
-loadBtnElement.addEventListener("click", async function () {
+loadBtnElement.addEventListener("click", loadPresents);
+
+//add present btn functionality
+addPresentBtnElement.addEventListener("click", async function () {
+  //no validations are needed per task description
+  try {
+    const postResponse = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        gift: giftInputElement.value,
+        for: forInputElement.value,
+        price: priceInputElement.value,
+      }),
+    });
+
+    if (!postResponse.ok) {
+      throw new Error("Error creating new present!");
+    }
+
+    formElement.reset();
+    loadPresents();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function createGiftItem(obj) {
+  let giftItem = sampleGiftItemElement.cloneNode(true);
+  giftItem.querySelector(".content > p:first-of-type").textContent = obj.gift;
+  giftItem.querySelector(".content > p:nth-child(even)").textContent = obj.for;
+  giftItem.querySelector(".content > p:last-of-type").textContent = obj.price;
+  giftItem.querySelector("button.change-btn").setAttribute("data-id", obj._id);
+  giftItem.querySelector("button.delete-btn").setAttribute("data-id", obj._id);
+  return giftItem;
+}
+
+async function loadPresents() {
+  //clear up the gift list
+  giftListElement.innerHTML = "";
   try {
     const response = await fetch(baseUrl);
 
@@ -29,14 +75,4 @@ loadBtnElement.addEventListener("click", async function () {
   } catch (error) {
     console.error(error);
   }
-});
-
-function createGiftItem(obj) {
-  let giftItem = sampleGiftItemElement.cloneNode(true);
-  giftItem.querySelector(".content > p:first-of-type").textContent = obj.gift;
-  giftItem.querySelector(".content > p:nth-child(even)").textContent = obj.for;
-  giftItem.querySelector(".content > p:last-of-type").textContent = obj.price;
-  giftItem.querySelector("button.change-btn").setAttribute("data-id", obj._id);
-  giftItem.querySelector("button.delete-btn").setAttribute("data-id", obj._id);
-  return giftItem;
 }
