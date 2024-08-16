5e1d52d5fbd0146f5ab6996b413202aecf91c379
Denitsa <denitsabebrevenska@gmail.com>
2024-08-16 17:52:03 +0300

    Product controller, view and model added

--- ASP.NET/MVCIntro/MVCIntro/MVCIntro/Controllers/ProductController.cs
+++ ASP.NET/MVCIntro/MVCIntro/MVCIntro/Controllers/ProductController.cs
@@ -1,10 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntro.Models.Product;
 
 namespace MVCIntro.Controllers;
 public class ProductController : Controller
 {
-    public IActionResult Index()
+    private IEnumerable<ProductViewModel> _products =
+        new List<ProductViewModel>()
+        {
+            new ()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 5.6m
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Coke",
+                Price = 2.3m
+            },
+            new()
+            {
+                Id = 3,
+                Name = "Flour",
+                Price = 1m
+            }
+        };
+
+    public IActionResult All()
     {
-        return View();
+        return View(_products);
+    }
+
+    public IActionResult GetProductById(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return BadRequest();
+        }
+
+        return View(product);
     }
 }
