cb6fddda647b36fbe180aff5c0c6364239cb56e0
Denitsa <denitsabebrevenska@gmail.com>
2024-08-16 17:03:30 +0300

    playing around with the mvc, all homecontroller methods and views implemented

---
+++ ASP.NET/MVCIntro/MVCIntro/MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntro.Models;
+using System.Diagnostics;
+
+namespace MVCIntro.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Message = "Hello World!";
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        ViewBag.Title = "AboutPage";
+        ViewBag.Message = "This is an ASP.NET Core MVC app.";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Numbers()
+    {
+        ViewBag.Tittle = "Nums 1 ... 50";
+        return View();
+    }
+
+    public IActionResult NumbersToN(int n = 3)
+    {
+        ViewBag.Number = n;
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASP.NET/MVCIntro/MVCIntro/MVCIntro/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro.Controllers;
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
