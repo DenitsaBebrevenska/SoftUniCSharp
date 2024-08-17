91f74318511d32cfacd033637035c2a959c41dea
Denitsa <denitsabebrevenska@gmail.com>
2024-08-17 14:58:19 +0300

    add meal implemented

--- JSFrontEnd/ExamPrep/RegularExam16Dec2023/03.DailyCalorieCounter/app.js
+++ JSFrontEnd/ExamPrep/RegularExam16Dec2023/03.DailyCalorieCounter/app.js
@@ -1,15 +1,29 @@
 const baseUrl = "http://localhost:3030/jsonstore/tasks";
+//target needed elements
 let mealListElement = document.getElementById("list");
+let formElement = document.querySelector("div#form > form");
+let foodInputElement = document.getElementById("food");
+let timeInputElement = document.getElementById("time");
+let caloeriesInputElement = document.getElementById("calories");
+let addMealBtnElement = document.getElementById("add-meal");
+let editMealBtnElement = document.getElementById("edit-meal");
+let loadBtnElement = document.getElementById("load-meals");
+
 //clone sample meal
 let sampleMeal = document.querySelector("div#list > div.meal").cloneNode(true);
 //clear sample meal
 mealListElement.innerHTML = "";
 
 //load meals
-let loadBtnElement = document.getElementById("load-meals");
 loadBtnElement.addEventListener("click", loadMeals);
 
+//add meal
+addMealBtnElement.addEventListener("click", addMeal);
+
 async function loadMeals() {
+  //flush list
+  mealListElement.innerHTML = "";
+
   try {
     const getResponse = await fetch(baseUrl);
 
@@ -38,6 +52,43 @@ async function loadMeals() {
     });
 
     mealListElement.appendChild(fragment);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addMeal() {
+  if (
+    !foodInputElement.value.length ||
+    !timeInputElement.value.length ||
+    !caloeriesInputElement.value.length
+  ) {
+    throw new Error("All fields must be filled in!");
+  }
+
+  let food = foodInputElement.value;
+  let time = timeInputElement.value;
+  let calories = caloeriesInputElement.value;
+
+  try {
+    const postResponse = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        food,
+        calories,
+        time,
+      }),
+    });
+
+    if (!postResponse.ok) {
+      throw new Error("Error creating meal!");
+    }
+
+    formElement.reset();
+    loadMeals();
   } catch (error) {
     console.error(error);
   }
