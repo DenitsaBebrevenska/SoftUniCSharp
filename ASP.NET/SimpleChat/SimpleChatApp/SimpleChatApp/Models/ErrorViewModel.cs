f0453d3b3afb68291d6af293d29311dac1c82ada
Denitsa <denitsabebrevenska@gmail.com>
2024-08-22 15:06:09 +0300

    chat app skeleton added

---
+++ ASP.NET/SimpleChat/SimpleChatApp/SimpleChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimpleChatApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
