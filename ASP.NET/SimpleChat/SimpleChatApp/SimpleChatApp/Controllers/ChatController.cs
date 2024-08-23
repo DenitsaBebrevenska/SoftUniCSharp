bff9efbfec041571d81f2759f205007f53889a46
Denitsa <denitsabebrevenska@gmail.com>
2024-08-23 20:03:00 +0300

    chat methods and views added

--- ASP.NET/SimpleChat/SimpleChatApp/SimpleChatApp/Controllers/ChatController.cs
+++ ASP.NET/SimpleChat/SimpleChatApp/SimpleChatApp/Controllers/ChatController.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleChatApp.Models.Message;
 
 namespace SimpleChatApp.Controllers;
 public class ChatController : Controller
 {
-    //just for the task, that is a bad practice
-    private static IList<KeyValuePair<string, string>> _messages =
-        new List<KeyValuePair<string, string>>();
+	//just for the task, that is a bad practice
+	private static readonly IList<KeyValuePair<string, string>> Messages =
+		new List<KeyValuePair<string, string>>();
 
-    public IActionResult Show()
-    {
-        return View();
-    }
+	[HttpGet]
+	public IActionResult Show()
+	{
+		if (!Messages.Any())
+		{
+			return View(new ChatViewModel());
+		}
 
-    [HttpPost]
-    public IActionResult Send()
-    {
-        return RedirectToAction("Show");
-    }
+		var chatModel = new ChatViewModel()
+		{
+			Messages = Messages
+				.Select(m => new MessageViewModel()
+				{
+					Sender = m.Key,
+					Text = m.Value
+				})
+				.ToList()
+		};
+
+		return View(chatModel);
+	}
+
+	[HttpPost]
+	public IActionResult Send(ChatViewModel chatModel)
+	{
+		var newMessage = chatModel.CurrentMessage;
+		Messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.Text));
+
+		return RedirectToAction("Show");
+	}
 }
