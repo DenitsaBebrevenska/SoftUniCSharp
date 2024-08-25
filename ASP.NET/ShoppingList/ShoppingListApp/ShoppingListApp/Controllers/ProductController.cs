e23a59c02597ea63eeb1d41db3244a19360c25ae
Denitsa <denitsabebrevenska@gmail.com>
2024-08-26 00:09:36 +0300

    added cganfed so views in home and produyct

--- ASP.NET/ShoppingList/ShoppingListApp/ShoppingListApp/Controllers/ProductController.cs
+++ ASP.NET/ShoppingList/ShoppingListApp/ShoppingListApp/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingListApp.Contracts;
+using ShoppingListApp.Models;
 
 namespace ShoppingListApp.Controllers;
 public class ProductController : Controller
@@ -10,9 +11,39 @@ public class ProductController : Controller
 	{
 		_service = service;
 	}
+
+	[HttpGet]
 	public async Task<IActionResult> Index()
 	{
 		var products = await _service.GetProductsAsync();
 		return View(products);
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetById(int id)
+	{
+		var product = await _service.GetProductAsync(id);
+		return View(product);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> AddProduct(ProductViewModel model)
+	{
+		await _service.AddProductAsync(model);
+		return View(model);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> UpdateProduct(ProductViewModel model)
+	{
+		await _service.UpdateProductAsync(model, model.Id);
+		return RedirectToAction(nameof(Index));
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> DeleteProduct(int id)
+	{
+		await _service.DeleteProductAsync(id);
+		return RedirectToAction(nameof(Index));
 	}
 }
