0ce7c4a38d99f0e4b30d6cc64a38fffd49b898c6
Denitsa <denitsabebrevenska@gmail.com>
2024-08-26 11:54:51 +0300

    added seed data and migrations, db context finished

--- ASP.NET/Forum/ForumApp/ForumApp.Infrastructure/Data/ForumAppDbContext.cs
+++ ASP.NET/Forum/ForumApp/ForumApp.Infrastructure/Data/ForumAppDbContext.cs
@@ -12,4 +12,15 @@ public class ForumAppDbContext : DbContext
 	}
 
 	public DbSet<Post> Posts { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Post>()
+			.HasData(
+				new Post() { Id = 1, Tittle = "First", Content = "My first post!" },
+				new Post() { Id = 2, Tittle = "Second", Content = "My second post!" },
+				new Post() { Id = 3, Tittle = "Third", Content = "My third post!" }
+				);
+		base.OnModelCreating(modelBuilder);
+	}
 }
---
+++ ASP.NET/Forum/ForumApp/ForumApp.Infrastructure/Migrations/20240826085323_SeedDataPosts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumApp.Infrastructure.Migrations
+{
+    public partial class SeedDataPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Tittle" },
+                values: new object[] { 1, "My first post!", "First" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Tittle" },
+                values: new object[] { 2, "My second post!", "Second" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Tittle" },
+                values: new object[] { 3, "My third post!", "Third" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
