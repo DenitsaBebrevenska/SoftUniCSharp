8df2ca9a4aae493cea49e82c1e68a2fb15f87306
Denitsa <denitsabebrevenska@gmail.com>
2024-09-02 23:45:40 +0300

    custom attributes added. Added view models and some logic into add method of game controller

---
+++ ASP.NET/ExamPrep/GameZone/GameZone/Attributes/IsCorrectDateFormat.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GameZone.Attributes;
+
+public class IsCorrectDateFormat : ValidationAttribute
+{
+    private readonly string _dateFormat;
+
+    public IsCorrectDateFormat(string dateFormat)
+    {
+        _dateFormat = dateFormat;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is string dateString &&
+            DateTime.TryParseExact(
+                dateString,
+                _dateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out DateTime dateResult))
+        {
+            return ValidationResult.Success!;
+        }
+
+        var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+        return new ValidationResult(errorMessage);
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return string.Format(ErrorMessageString
+                             ?? $"The {name} field must be a valid date in {_dateFormat} format.", name, _dateFormat);
+    }
+}
