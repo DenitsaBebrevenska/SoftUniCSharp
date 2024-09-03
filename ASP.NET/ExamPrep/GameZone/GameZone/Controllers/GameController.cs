65b34a018434a637be33ffd41e90881aec622ba4
Denitsa <denitsabebrevenska@gmail.com>
2024-09-03 14:59:47 +0300

    delete and edit methods and views implemented

--- ASP.NET/ExamPrep/GameZone/GameZone/Controllers/GameController.cs
+++ ASP.NET/ExamPrep/GameZone/GameZone/Controllers/GameController.cs
@@ -160,6 +160,160 @@ public class GameController : Controller
 		return RedirectToAction(nameof(MyZone));
 	}
 
+	[HttpGet]
+	public async Task<IActionResult> Edit(int id)
+	{
+		var game = await _context
+			.Games
+			.FindAsync(id);
+
+		if (game == null)
+		{
+			return BadRequest();
+		}
+
+		if (game.PublisherId != GetCurrentUserId())
+		{
+			return Unauthorized();
+		}
+
+		var model = new GameFormViewModel()
+		{
+			Id = game.Id,
+			Title = game.Title,
+			Description = game.Description,
+			ImageUrl = game.ImageUrl,
+			ReleasedOn = game.ReleasedOn.ToString(DefaultDateTimeFormat),
+			GenreId = game.GenreId,
+			Genres = await GetAvailableGenreModels()
+		};
+
+		return View(model);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Edit(int id, GameFormViewModel model)
+	{
+		var game = await _context
+			.Games
+			.FindAsync(id);
+
+		if (game == null)
+		{
+			return BadRequest();
+		}
+
+		if (game.PublisherId != GetCurrentUserId())
+		{
+			return Unauthorized();
+		}
+
+		if (!ModelState.IsValid)
+		{
+			return View(model);
+		}
+
+		game.Title = model.Title;
+		game.ImageUrl = model.ImageUrl;
+		game.Description = model.Description;
+		game.ReleasedOn = DateTime.ParseExact(model.ReleasedOn, DefaultDateTimeFormat, CultureInfo.InvariantCulture);
+		game.GenreId = model.GenreId;
+
+		await _context.SaveChangesAsync();
+
+		return RedirectToAction(nameof(All));
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Details(int id)
+	{
+		var game = await _context
+			.Games
+			.Include(g => g.Publisher)
+			.Include(g => g.Genre)
+			.FirstOrDefaultAsync(g => g.Id == id);
+
+		if (game == null)
+		{
+			return BadRequest();
+		}
+
+		var model = new GameDetailsViewModel()
+		{
+			Id = id,
+			Title = game.Title,
+			ImageUrl = game.ImageUrl,
+			Description = game.Description,
+			ReleasedOn = game.ReleasedOn.ToString(DefaultDateTimeFormat),
+			Genre = game.Genre.Name,
+			Publisher = game.Publisher.UserName
+		};
+
+		return View(model);
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Delete(int id)
+	{
+		var game = await _context
+			.Games
+			.Include(g => g.Publisher)
+			.FirstOrDefaultAsync(g => g.Id == id);
+
+		if (game == null)
+		{
+			return BadRequest();
+		}
+
+		if (game.PublisherId != GetCurrentUserId())
+		{
+			return Unauthorized();
+		}
+
+		var model = new GameDeleteViewModel()
+		{
+			Id = game.Id,
+			Title = game.Title,
+			Description = game.Description,
+			Publisher = game.Publisher.UserName
+		};
+
+		return View(model);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> DeleteConfirmed(int id)
+	{
+		var game = await _context
+			.Games
+			.FirstOrDefaultAsync(g => g.Id == id);
+
+		if (game == null)
+		{
+			return BadRequest();
+		}
+
+		if (game.PublisherId != GetCurrentUserId())
+		{
+			return Unauthorized();
+		}
+
+		var gamersGames = await _context
+			.GamersGames
+			.Where(gg => gg.GameId == id)
+			.ToListAsync();
+
+		_context.GamersGames
+			.RemoveRange(gamersGames);
+
+		_context
+			.Games
+			.Remove(game);
+
+		await _context.SaveChangesAsync();
+
+		return RedirectToAction(nameof(All));
+	}
 	private async Task<ICollection<GenreFormViewModel>> GetAvailableGenreModels()
 		=> await _context
 			.Genres
---
+++ ASP.NET/ExamPrep/GameZone/GameZone/Models/Game/GameDeleteViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GameZone.Models.Game;
+
+public class GameDeleteViewModel
+{
+	public int Id { get; set; }
+
+	public string Title { get; set; } = null!;
+
+	public string Description { get; set; } = null!;
+
+	public string Publisher { get; set; } = null!;
+}
