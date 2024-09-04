2b19ae30d08b811470f8dedc65245338a82cbfbe
Denitsa <denitsabebrevenska@gmail.com>
2024-09-05 00:28:39 +0300

    moved date to infrastructure

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/HomeRentingDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data;
+public class HomeRentingDbContext : IdentityDbContext
+{
+	public HomeRentingDbContext(DbContextOptions<HomeRentingDbContext> options)
+		: base(options)
+	{
+	}
+}
