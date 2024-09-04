7a69725e3a7d38c31079f178969f065cb61a9283
Denitsa <denitsabebrevenska@gmail.com>
2024-09-05 00:17:19 +0300

    service collection  extension added

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		return services;
+	}
+
+	public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+	{
+		var connectionString = config.GetConnectionString("DefaultConnection") ??
+							   throw new ArgumentException("DefaultConnection string not set.");
+		services.AddDbContext<HomeRentingDbContext>(options => options.UseSqlServer(connectionString));
+
+		services.AddDatabaseDeveloperPageExceptionFilter();
+
+		return services;
+	}
+
+	public static IServiceCollection AddApplicationIdentity(this IServiceCollection services)
+	{
+		services.AddDefaultIdentity<IdentityUser>(options =>
+			{
+				options.SignIn.RequireConfirmedAccount = false;
+				options.Password.RequireUppercase = false;
+				options.Password.RequireNonAlphanumeric = false;
+			})
+			.AddEntityFrameworkStores<HomeRentingDbContext>();
+
+		return services;
+	}
+}
