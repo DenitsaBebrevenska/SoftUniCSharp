c6c3d0b2d4bc6278937c4334ccbeb56abceb10d2
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 17:19:09 +0300

    Become model and view added

--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,5 +1,15 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent;
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.ValidationMessages;
+using static HouseRentingSystem.Infrastructure.Data.Constants.TableConstraints;
+namespace HouseRentingSystem.Core.Models.Agent;
 
 public class BecomeAgentFormModel
 {
+    [Required(ErrorMessage = RequiredFieldMessage)]
+    [StringLength(AgentPhoneNumberMaxLength,
+        MinimumLength = AgentPhoneNumberMinLength,
+        ErrorMessage = StringLengthMessage)]
+    [Display(Name = "Phone number")]
+    [Phone]
+    public string PhoneNumber { get; init; } = null!;
 }
