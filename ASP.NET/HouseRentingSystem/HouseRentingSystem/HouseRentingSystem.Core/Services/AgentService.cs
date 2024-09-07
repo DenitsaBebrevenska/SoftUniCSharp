f2a1e528cdffa362ab1f7f4b7609a7a9d72f6381
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 17:09:03 +0300

    Agent service fully implemented

--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Services/AgentService.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Services/AgentService.cs
@@ -1,6 +1,42 @@
 ﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Infrastructure.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Core.Services;
 public class AgentService : IAgentService
 {
+    private readonly IRepository _repository;
+
+    public AgentService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<bool> ExistsByIdAsync(string userId)
+        => await _repository
+            .GetAllReadOnly<Agent>()
+            .AnyAsync(a => a.UserId == userId);
+
+    public async Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
+        => await _repository
+            .GetAllReadOnly<Agent>()
+            .AnyAsync(a => a.PhoneNumber == phoneNumber);
+
+    public async Task<bool> UserHasRentsAsync(string userId)
+        => await _repository
+            .GetAllReadOnly<House>()
+            .AnyAsync(h => h.RenterId == userId);
+
+    public async Task CreateAsync(string userId, string phoneNumber)
+    {
+        var agent = new Agent()
+        {
+            PhoneNumber = phoneNumber,
+            UserId = userId
+        };
+
+        await _repository.AddAsync(agent);
+        await _repository.SaveChangesAsync();
+    }
 }
