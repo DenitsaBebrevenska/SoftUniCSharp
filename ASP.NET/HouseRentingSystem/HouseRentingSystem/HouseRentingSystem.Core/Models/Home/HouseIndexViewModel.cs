9cb5520077e70f2ee1bf1a5033190bfbac7fb274
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 14:58:32 +0300

    Home controller, view an models modified

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Models/Home/HouseIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseRentingSystem.Core.Models.Home;
+
+public class HouseIndexViewModel
+{
+	public int Id { get; set; }
+
+	public string Title { get; set; } = null!;
+
+	public string ImageUrl { get; set; } = null!;
+}
--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,19 +1,26 @@
-﻿using HouseRentingSystem.Core.Models;
-using HouseRentingSystem.Core.Models.Home;
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HouseRentingSystem.Controllers;
 public class HomeController : Controller
 {
-	public async Task<IActionResult> Index()
-	{
-		return View(new IndexHomeViewModel());
-	}
+    private readonly IHouseService _houseService;
 
-	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-	public IActionResult Error()
-	{
-		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-	}
+    public HomeController(IHouseService houseService)
+    {
+        _houseService = houseService;
+    }
+
+    public async Task<IActionResult> Index()
+        => View(await _houseService
+            .LastThreeHousesAsync());
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
