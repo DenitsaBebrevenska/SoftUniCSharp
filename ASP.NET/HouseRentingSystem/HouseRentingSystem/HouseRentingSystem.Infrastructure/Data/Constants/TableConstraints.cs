b40cd291978af324cffe2ab4b9ebbbedafa2f2a6
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 21:26:24 +0300

    the range constraints for decimal converted to string

--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Constants/TableConstraints.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Constants/TableConstraints.cs
@@ -1,27 +1,27 @@
 ﻿namespace HouseRentingSystem.Infrastructure.Data.Constants;
 public static class TableConstraints
 {
-	public const int CategoryNameMaxLength = 50;
+    public const int CategoryNameMaxLength = 50;
 
-	public const int HouseTitleMinLength = 10;
+    public const int HouseTitleMinLength = 10;
 
-	public const int HouseTitleMaxLength = 50;
+    public const int HouseTitleMaxLength = 50;
 
-	public const int HouseAddressMinLength = 30;
+    public const int HouseAddressMinLength = 30;
 
-	public const int HouseAddressMaxLength = 150;
+    public const int HouseAddressMaxLength = 150;
 
-	public const int HouseDescriptionMinLength = 50;
+    public const int HouseDescriptionMinLength = 50;
 
-	public const int HouseDescriptionMaxLength = 500;
+    public const int HouseDescriptionMaxLength = 500;
 
-	public const decimal HousePricePerMonthMinimum = 0m;
+    public const string HousePricePerMonthMinimum = "0";
 
-	public const decimal HousePricePerMonthMaximum = 2_000m;
+    public const string HousePricePerMonthMaximum = "2000";
 
-	public const int AgentPhoneNumberMinLength = 7;
+    public const int AgentPhoneNumberMinLength = 7;
 
-	public const int AgentPhoneNumberMaxLength = 15;
+    public const int AgentPhoneNumberMaxLength = 15;
 
-	public const int ImageUrlMaxLength = 2_048;
+    public const int ImageUrlMaxLength = 2_048;
 }
