d78b606356c6362fb9a148d472295d15953f801e
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 17:19:19 +0300

    validation messages added

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Constants/ValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace HouseRentingSystem.Core.Constants;
+public static class ValidationMessages
+{
+    public const string RequiredFieldMessage = "The {0} field is required.";
+
+    public const string StringLengthMessage = "The field {0} must be between {2} and {1} characters long.";
+}
