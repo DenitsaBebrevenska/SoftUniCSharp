e2b34ccdd720d1135cbb22dd8e4246eebdf305e9
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 14:59:33 +0300

    repository added, unit of work for the repository. not the repo pattern

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Infrastructure.Common;
+public interface IRepository
+{
+    IQueryable<T> GetAll<T>() where T : class;
+
+    IQueryable<T> GetAllReadOnly<T>() where T : class;
+
+    int SaveChanges();
+}
---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/Repository.cs
@@ -0,0 +1,26 @@
+﻿using HouseRentingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Common;
+public class Repository : IRepository
+{
+	private readonly HomeRentingDbContext _context;
+
+	public Repository(HomeRentingDbContext context)
+	{
+		_context = context;
+	}
+
+	private DbSet<T> GetDbSet<T>() where T : class
+		=> _context.Set<T>();
+
+	public IQueryable<T> GetAll<T>() where T : class
+		=> GetDbSet<T>();
+
+	public IQueryable<T> GetAllReadOnly<T>() where T : class
+		=> GetDbSet<T>()
+			.AsNoTracking();
+
+	public int SaveChanges()
+	 => _context.SaveChanges();
+}
