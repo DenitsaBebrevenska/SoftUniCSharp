c4f3fcb6f7c64760a1ae039ceb510814eef67ef7
Denitsa <denitsabebrevenska@gmail.com>
2024-09-07 18:26:32 +0300

    validation messages renamed to validation error messages

---
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem.Core/Constants/ValidationErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace HouseRentingSystem.Core.Constants;
+public static class ValidationErrorMessages
+{
+    public const string RequiredFieldMessage = "The {0} field is required.";
+
+    public const string StringLengthMessage = "The field {0} must be between {2} and {1} characters long.";
+
+    public const string PhoneNumberExistsMessage = "Phone number already exists. Please enter a new one.";
+
+    public const string UserHasRentsMessage = "Cannot become agent if you have rents.";
+}
