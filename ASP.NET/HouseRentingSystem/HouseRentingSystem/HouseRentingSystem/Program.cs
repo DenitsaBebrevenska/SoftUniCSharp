4a981b6a604e25e104e82d616ae0fcd9295ca8ae
Denitsa <denitsabebrevenska@gmail.com>
2024-09-13 09:54:02 +0300

    redirects are needed, reexcution does not work without exceptions for custom status code pages

--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Program.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 else
 {
 	app.UseExceptionHandler("/Home/Error/500");
-	app.UseStatusCodePagesWithReExecute("/Home/Error?statusCode={0}");
+	app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 	app.UseHsts();
 }
 
