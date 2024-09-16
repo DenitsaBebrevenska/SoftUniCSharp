f07912de7d2bf809ea79c98f08243b6a7ca2331a
Denitsa <denitsabebrevenska@gmail.com>
2024-09-16 13:52:17 +0300

    ClaimsPrincipalExtension further extended

--- ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Extensions/ClaimsPrincipalExtension.cs
+++ ASP.NET/HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Extensions/ClaimsPrincipalExtension.cs
@@ -1,8 +1,12 @@
-﻿namespace System.Security.Claims;
+﻿using static HouseRentingSystem.Core.Constants.RoleConstants;
+namespace System.Security.Claims;
 
 public static class ClaimsPrincipalExtension
 {
     public static string Id(this ClaimsPrincipal user)
     => user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+    public static bool IsAdmin(this ClaimsPrincipal user)
+        => user.IsInRole(AdminRole);
 
 }
